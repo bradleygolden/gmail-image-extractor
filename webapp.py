3696319f61fe8a58ed8fd5b85cce8493b7224e49
Bradley Golden <bradleygolden@Bradleys-MacBook-Pro.local>
2015-08-21 18:44:47 -0500

    configuring for nginx

--- webapp.py
+++ webapp.py
@@ -1,25 +1,31 @@
+#!/usr/bin/env python
+
+import os.path
+
 import tornado
 import tornado.web
 import tornado.template
 import tornado.websocket
-import tornado.gen
 import tornado.auth
 import tornado.escape
-import os
-from os.path import expanduser
+
+import logging
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client import client
+from apiclient.discovery import build
+from apiclient import errors
+
+import httplib2
+
 from gmailextract.extractor import GmailImageExtractor
 import config
 
 from tornado.options import define, options
 define("port", default=config.port, help="run on the given port", type=int)
 
-root_dir = os.path.dirname(os.path.abspath(__file__))
-attr_dir = os.path.join(expanduser("~"), "Gmail Images")
-if not os.path.isdir(attr_dir):
-    os.mkdir(attr_dir)
-
-# tpl_loader = tornado.template.Loader(os.path.join(root_dir, 'templates'))
+attr_dir = os.path.dirname(os.path.abspath(__file__))
 state = {}
+images = []
 
 
 def plural(msg, num):
@@ -32,84 +38,132 @@ def plural(msg, num):
 class Application(tornado.web.Application):
     def __init__(self):
 
+        # TODO - add static file location at root
         handlers = [
             # handlers
             (r"/", MainHandler),
-            (r'/ws', SocketHandler),
-            # (r"/auth/login", GoogleOAuth2LoginHandler),
-            # (r"/login", LoginHandler),
-            # (r"/logout", LogoutHandler),
-
-            # static paths
-            # (r"/assets/(.*)", tornado.web.StaticFileHandler, (dict(path=settings['static_path']))),
-            # (r"/downloads/(.*)", tornado.web.StaticFileHandler, (dict(path=settings['static_path']))),
+            (r"/login", LoginHandler),
+            (r"/logout", LogoutHandler),
+            (r"/auth/login", GoogleOAuth2LoginHandler),
+            (r"/extractor", ExtractorHandler),
+            (r"/ws", SocketHandler),
+            # TODO - Serve files outside of namespace of server
+            (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "/user_downloads"}),
+            (r'/download/(.*)', tornado.web.StaticFileHandler,
+             dict(path='/Users/bradleygolden/Gmail-Image-Extractor/download')),
+            (r'/check', CheckHandler),
+            (r'/protected/([a-zA-Z0-9_-]+$)', ProtectedHandler),
         ]
 
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), 'templates'),
             static_path=os.path.join(os.path.dirname(__file__), 'static'),
-            login_url=config.oauth2_login_uri,
-            redirect_uri=config.oauth2_redirect_uri,
-            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            ui_modules={'DeleteModal': DeleteModalModule,
+                        'ImageModal': ImageModalModule,
+                        'ImageThumbnail': ImageThumbnailModule,
+                        'ImageMenu': ImageMenuModule},
+            debug=config.debug,
+            login_url=config.oauth2_login_url,
+            redirect_uri=config.oauth2_redirect_url,
+            cookie_secret=config.cookie_secret,
             xsrf_cookies=config.xsrf_cookies,
             google_oauth={"key": config.oauth2_client_id, "secret": config.oauth2_client_secret},
-            debug=config.debug,
         )
 
         tornado.web.Application.__init__(self, handlers, **settings)
 
 
+class CheckHandler(tornado.web.RequestHandler):
+    def get(self):
+        serve_cookie = self.set_secure_cookie('serve_cookie')
+        if serve_cookie:
+            self.redirect('/protected/a')
+        else:
+            # TODO - redirect to 404 error
+            pass
+
+
+class ProtectedHandler(tornado.web.RequestHandler):
+    def get(self, file):
+        serve_cookie = self.get_secure_cookie('serve_cookie')
+        if serve_cookie:
+            self.set_header('X-Accel-Redirect', ''.join(('/protected/', file, '.html')))
+            self.finish()
+
+
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
-        user_json = self.get_secure_cookie('user')
-        if not user_json:
+        user = self.get_secure_cookie('user')
+        if not user:
             return None
-        return True
+
+        # TODO - Implement after clicking "begin"
+        # credentials = client.OAuth2Credentials.from_json(user)
+        # if credentials.access_token_expired:
+            # self.redirect(self.settings['login_url'])
 
 
 class MainHandler(BaseHandler):
-    # @tornado.web.authenticated
-
     def get(self):
-        self.render('main.html')
+        self.render('index.html', site_name=config.site_name, site_description=config.description)
 
 
 class LoginHandler(BaseHandler):
-    # @tornado.web.authenticated
     def get(self):
-        self.render('main.html')
+        self.redirect(self.settings['login_url'])
 
-class LogoutHandler(BaseHandler):
-    @tornado.web.authenticated
+
+# TODO - fix logout process
+class LogoutHandler(tornado.web.RequestHandler):
     def get(self):
-        self.clear_cookie('user')
-        self.redirect("/")
+        user = self.get_secure_cookie('user')
+        if user:
+            credentials = client.OAuth2Credentials.from_json(user)
+            credentials.revoke(httplib2.Http())
+            self.clear_all_cookies()
+        self.redirect('/')
 
 
 class GoogleOAuth2LoginHandler(tornado.web.RequestHandler,
                                tornado.auth.GoogleOAuth2Mixin):
     @tornado.gen.coroutine
     def get(self):
-        if self.get_argument('code', False):
-            user = yield self.get_authenticated_user(
-                redirect_uri='http://localhost:8888/auth/login',
-                code=self.get_argument('code'))
-            # Save the user with e.g. set_secure_cookie
-            self.set_secure_cookie('user', tornado.escape.json_encode(user))
-            self.redirect("/")
+        flow = OAuth2WebServerFlow(client_id=self.settings['google_oauth']['key'],
+                                   client_secret=self.settings['google_oauth']['secret'],
+                                   scope=['email', 'https://mail.google.com'],
+                                   redirect_uri=self.settings['redirect_uri'])
 
+        if not self.get_argument('code', False):
+            auth_uri = flow.step1_get_authorize_url()
+            self.redirect(auth_uri)
         else:
-            yield self.authorize_redirect(
-                redirect_uri='http://localhost:8888/auth/login',
-                client_id=self.settings['google_oauth']['key'],
-                # scope for full access to email:https://mail.google.com/
-                # scope for modifying emails: https://www.googleapis.com/auth/userinfo.email
-                scope=['profile', 'email', 'https://mail.google.com/'],
-                response_type='code',
-                extra_params={'approval_prompt': 'auto'})
+            auth_code = self.get_argument('code')
+            credentials = flow.step2_exchange(auth_code)
+            user_info = get_user_info(credentials)
+            access_token = credentials.access_token
+            email = user_info['email']
+
+            self.set_secure_cookie('user', credentials.to_json())
+            self.set_secure_cookie('email', email)
+            self.set_secure_cookie('access_token', access_token)
+            self.redirect('/extractor')
+
+
+class ExtractorHandler(tornado.web.RequestHandler):
+    def get(self):
+        access_token = self.get_secure_cookie('access_token')
+        if access_token:
+            self.render('extract.html', site_name=config.site_name)
+        else:
+            self.redirect('/')
 
 
 class SocketHandler(tornado.websocket.WebSocketHandler):
+
+    def open(self):
+        self.write_message({'ok': True,
+                            "type": "ws-open",
+                            "msg": u"Waiting for the server..."})
 
     def on_message(self, message):
         msg = tornado.escape.json_decode(message)
@@ -117,24 +171,26 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
             return
         elif msg['type'] == 'connect':
             self._handle_connect(msg)
-        elif msg['type'] == 'sync':
-            self._handle_sync(msg)
-        elif msg['type'] == 'confirm':
-            self._handle_confirmation(msg)
+        # elif msg['type'] == 'sync':
+        #    self._handle_sync(msg)
+        # elif msg['type'] == 'confirm':
+        #    self._handle_confirmation(msg)
         elif msg['type'] == 'delete':
             self._handle_delete(msg)
         elif msg['type'] == 'save':
             self._handle_save(msg)
+        elif msg['type'] == 'remove-zip':
+            self._handle_remove_zip(msg)
         else:
             return
 
     def _handle_connect(self, msg, callback=None):
 
-        # obtain user cookie
-        user_json = self.get_secure_cookie('user')
+        access_token = self.get_secure_cookie('access_token')
+        email = self.get_secure_cookie('email')
 
-        state['extractor'] = GmailImageExtractor(attr_dir, msg['email'],
-                                                 msg['pass'], limit=int(msg['limit']),
+        state['extractor'] = GmailImageExtractor(attr_dir, email,
+                                                 access_token, limit=int(msg['limit']),
                                                  batch=int(msg['simultaneous']),
                                                  replace=bool(msg['rewrite']))
         if not state['extractor'].connect():
@@ -160,7 +216,8 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
                                         "type": "image",
                                         "msg_id": args[1],
                                         "img_id": args[2],
-                                        "enc_img": args[3]})
+                                        "enc_img": args[3],
+                                        "img_name": args[4]})
 
                 if args[0] == 'message':
                     status_msg = u"Fetching messages {1} - {2}".format(msg['simultaneous'],
@@ -170,12 +227,27 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
                                         "msg": status_msg,
                                         "num": args[1]})
 
-            attachment_count = state['extractor'].extract(_status)
-            self.write_message({"ok": True,
-                                "type": "download-complete",
-                                "msg": "Succesfully found {0} {1}"
-                                "".format(attachment_count, plural(u"image", attachment_count)),
-                                "num": attachment_count})
+            # TODO - Run extract process on different thread
+            def _extract():
+                attachment_count = state['extractor'].extract(_status)
+
+                self.write_message({"ok": True,
+                                    "type": "download-complete",
+                                    "msg": "Succesfully found {0} {1}"
+                                    "".format(attachment_count, plural(u"image", attachment_count)),
+                                    "num": attachment_count})
+
+            loop = tornado.ioloop.IOLoop.instance()
+            loop.add_callback(_extract)
+
+            # attachment_count = state['extractor'].extract(_status)
+            # attachment_count = state['extractor'].get_attachment_count()
+
+            # self.write_message({"ok": True,
+            # "type": "download-complete",
+            # "msg": "Succesfully found {0} {1}"
+            # "".format(attachment_count, plural(u"image", attachment_count)),
+            # "num": attachment_count})
 
     def _handle_delete(self, msg):
         extractor = state['extractor']
@@ -231,21 +303,72 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
                                     "images": args[1],
                                     "image_names": args[2]})
 
-            if update_type == "write-zip":
-                write_zip_passed = args[1]
+            if update_type == "saved-zip":
+                save_zip_passed = args[1]
                 file_name = args[2]
+                # file_link = sha256(file_name).hexdigest()
+                file_link = file_name
 
-                if write_zip_passed:
+                download_path = "/download/" + file_link
+
+                if save_zip_passed:
                     self.write_message({"ok": True,
-                                        "link": u"""<a href="gmailextract/user_downloads/{0}">"""
+                                        "link": u"""<a href="{0}"
+                                        target="_blank" download="{3}">"""
                                         "Click Here to Download Your Gmail Images"
-                                        "</a>".format(file_name),
-                                        "type": "zip"})
+                                        "</a><span> (Your images will be available for"
+                                        " {2} {1} or <a id=""remove-now"" href=""#"">"
+                                        "remove them now</a>"
+                                        ")</span>"
+                                        "".format(download_path,
+                                                  plural(u"minute",
+                                                         config.zip_removal_countdown/60),
+                                                  config.zip_removal_countdown/60,
+                                                  file_name),
+                                        "type": "saved-zip"})
+
+                    def _remove_link():
+                        email = self.get_secure_cookie('email')
+                        extractor.remove_zip(email, _save_status)
+                        try:
+                            self.write_message({"ok": True,
+                                                "type": "removed-zip",
+                                                "msg": "Please check all attachments that"
+                                                " you want to remove from your Gmail account."})
+                        except:
+                            pass
+
+                    loop = tornado.ioloop.IOLoop.instance()
+                    # remove link and zip file after 30 min
+                    loop.call_later(config.zip_removal_countdown, _remove_link)
+                    # loop.call_later(5, _remove_link)
+
                 else:
                     self.write_message(self.write(
                         "<span> Failed to create zip file :( </span>"))
 
-        extractor.save(msg, _save_status)
+        email = self.get_secure_cookie('email')
+        extractor.save(msg, email, _save_status)
+
+    def _handle_remove_zip(self, msg):
+        extractor = state['extractor']
+
+        def _remove_status(*args):
+            update_type = args[0]
+            if update_type == "removed-zip":
+                removed_zip_passed = args[1]
+
+                if removed_zip_passed:
+                    try:
+                        self.write_message({"ok": True,
+                                            "type": "removed-zip",
+                                            "msg": "Please check all attachments that you want to"
+                                            " remove from your Gmail account."})
+                    except:
+                        pass
+
+        email = self.get_secure_cookie('email')
+        extractor.remove_zip(email, _remove_status)
 
     def _handle_sync(self, msg):
         extractor = state['extractor']
@@ -286,14 +409,68 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
                                       plural(u"message", num_msg_changed))})
 
     def on_close(self):
+        # remove the user's zip file
+        email = self.get_secure_cookie('email')
+        state['extractor'].remove_zip(email)
         state['extractor'] = None
+
+
+class DeleteModalModule(tornado.web.UIModule):
+    def render(self, id):
+        return self.render_string('modules/delete_modal.html', id=id)
+
+
+class ImageModalModule(tornado.web.UIModule):
+    def render(self, id):
+        return self.render_string('modules/image_modal.html', id=id)
+
+
+class ImageThumbnailModule(tornado.web.UIModule):
+    def render(self, image):
+        return self.render_string('modules/image_thumbnail.html', image=image)
+
+
+class ImageMenuModule(tornado.web.UIModule):
+    def render(self):
+        return self.render_string('modules/images_menu.html')
+
+
+class NoUserIdException(Exception):
+    """Error raised when no user ID could be retrieved."""
+    # print "No UserID could be retreived"
+
+
+def get_user_info(credentials):
+    """Send a request to the UserInfo API to retrieve the user's information.
+
+    Argss:
+        credentials: oauth2client.client.OAuth2Credentials instance to authorize the
+        request.
+
+    Returns:
+        User information as a dict.
+        """
+
+    user_info_service = build(
+        serviceName='oauth2', version='v2',
+        http=credentials.authorize(httplib2.Http()))
+    user_info = None
+    try:
+        user_info = user_info_service.userinfo().get().execute()
+        return user_info
+    except errors.HttpError, e:
+        logging.error('An error occurred: %s', e)
+        if user_info and user_info.get('id'):
+            return user_info
+        else:
+            raise NoUserIdException()
 
 
 def server_prompt():
     print ("-------------------------------------")
     print ("Base Url: {0}".format(config.base_url))
-    print ("Port: {0}".format(config.port))
-    print ("View at: {0}".format(config.full_url))
+    print ("Port: {0}".format(options.port))
+    print ("View at: {0}".format(config.base_url + ":" + str(options.port)))
     print ("-------------------------------------")
 
 
@@ -302,7 +479,7 @@ def main():
     application = Application()
     server_prompt()
     application.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+    tornado.ioloop.IOLoop.current().start()
 
 
 if __name__ == "__main__":
